e9c6de9f5d9cfd61f825f18ed8e3d75b84de7f7b
kc834 <34583525+kc834@users.noreply.github.com>
2020-03-09 18:15:07 -0400

    replaced arnoldi block with bilanczos
    also changed time condition

--- expvB.m
+++ expvB.m
@@ -1,1 +1,201 @@
+%  [w, err, hump] = expv( t, A, v, tol, m )
+%  EXPV computes an approximation of w = exp(t*A)*v for a
+%  general matrix A using Krylov subspace  projection techniques.
+%  It does not compute the matrix exponential in isolation but instead,
+%  it computes directly the action of the exponential operator on the 
+%  operand vector. This way of doing so allows for addressing large
+%  sparse problems. The matrix under consideration interacts only
+%  via matrix-vector products (matrix-free method).
+%
+%  w = expv( t, A, v )
+%  computes w = exp(t*A)*v using a default tol = 1.0e-7 and m = 30.
+%
+%  [w, err] = expv( t, A, v )
+%  renders an estimate of the error on the approximation.
+%
+%  [w, err] = expv( t, A, v, tol )
+%  overrides default tolerance.
+%
+%  [w, err, hump] = expv( t, A, v, tol, m )
+%  overrides default tolerance and dimension of the Krylov subspace,
+%  and renders an approximation of the `hump'.
+%
+%  The hump is defined as:
+%          hump = max||exp(sA)||, s in [0,t]  (or s in [t,0] if t < 0).
+%  It is used as a measure of the conditioning of the matrix exponential
+%  problem. The matrix exponential is well-conditioned if hump = 1,
+%  whereas it is poorly-conditioned if hump >> 1. However the solution
+%  can still be relatively fairly accurate even when the hump is large
+%  (the hump is an upper bound), especially when the hump and 
+%  ||w(t)||/||v|| are of the same order of magnitude (further details in 
+%  reference below).
+%
+%  Example 1:
+%  ----------
+%    n = 100;
+%    A = rand(n);
+%    v = eye(n,1);
+%    w = expv(1,A,v);
+%
+%  Example 2:
+%  ----------
+%    % generate a random sparse matrix
+%    n = 100;
+%    A = rand(n);
+%    for j = 1:n
+%        for i = 1:n
+%            if rand < 0.5, A(i,j) = 0; end;
+%        end;
+%    end;
+%    v = eye(n,1);
+%    A = sparse(A); % invaluable for a large and sparse matrix.
+%
+%    tic
+%    [w,err] = expv(1,A,v);
+%    toc
+%
+%    disp('w(1:10) ='); disp(w(1:10));
+%    disp('err =');     disp(err);
+%
+%    tic
+%    w_matlab = expm(full(A))*v;
+%    toc
+%
+%    disp('w_matlab(1:10) ='); disp(w_matlab(1:10));
+%    gap = norm(w-w_matlab)/norm(w_matlab);
+%    disp('||w-w_matlab|| / ||w_matlab|| ='); disp(gap);
+%
+%  In the above example, n could have been set to a larger value,
+%  but the computation of w_matlab will be too long (feel free to
+%  discard this computation).
+%
+%  See also MEXPV, EXPOKIT.
 
+%  Roger B. Sidje (rbs@maths.uq.edu.au)
+%  EXPOKIT: Software Package for Computing Matrix Exponentials. 
+%  ACM - Transactions On Mathematical Software, 24(1):130-156, 1998
+
+function [w, err, hump] = expvB( t, A, v, tol, m )
+
+% preprocessing
+[n,n] = size(A);
+if nargin == 3
+  tol = 1.0e-7;
+  m = min(n,30);
+end
+if nargin == 4
+  m = min(n,30);
+end
+
+anorm = norm(A,'inf'); % maximum absolute row sum
+mxrej = 10;
+btol  = 1.0e-7; 
+gamma = 0.9; 
+delta = 1.2; 
+mb    = m; % dimension
+t_out   = abs(t); % absolute value of time
+nstep = 0; 
+t_new   = 0;
+t_now = 0; 
+t_end = t_out; % Added to modify code for range
+s_error = 0;
+rndoff= anorm*eps;
+
+k1 = 2; 
+xm = 1/m; 
+normv = norm(v); % Euclidean norm 
+beta = normv; 
+fact = (((m+1)/exp(1))^(m+1))*sqrt(2*pi*(m+1));
+t_new = (1/anorm)*((fact*tol)/(4*beta*anorm))^xm;
+s = 10^(floor(log10(t_new))-1); 
+t_new = ceil(t_new/s)*s; 
+sgn = sign(t); 
+nstep = 0;
+k=m;
+
+w = v;
+hump = normv;
+while (t_end - t_now) > 0 % while range is high
+  nstep = nstep + 1;
+  t_step = min( t_out-t_now,t_new );
+  
+  n = length(A); % Max dim
+  Q = zeros(n,k+1); % Orthonormal basis, n by k+1 array
+  R = zeros(n,k+1);
+  T = zeros(k+1,k);
+  alpha = zeros(k,1); 
+  beta = zeros(k,1);
+  gamma = zeros(k,1);
+
+  Q(:,1) = b/norm(b); % Arbitrary vector with norm 1
+  Q(:,1) = Q(:,1)/norm(Q(:,1));
+  R(:,1) = Q(:,1);
+  for j = 1:k 
+      Q(:,j+1) = A*Q(:,j); % Move on to next vector in Krylov subspace
+      R(:,j+1) = A'*R(:,j);
+      alpha(j) = R(:,j)'*Q(:,j+1);
+      Q(:,j+1) = Q(:,j+1)-alpha(j)*Q(:,j); 
+      R(:,j+1) = R(:,j+1)-alpha(j)*R(:,j); 
+      if j > 1 
+          Q(:,j+1) = Q(:,j+1)-beta(j-1)*Q(:,j-1); 
+          R(:,j+1) = R(:,j+1)-gamma(j-1)*R(:,j-1); 
+      end
+      gamma(j) = norm(Q(:,j+1));
+      Q(:,j+1) = Q(:,j+1)/gamma(j);  
+      beta(j) = R(:,j+1)'*Q(:,j+1);
+      R(:,j+1) = R(:,j+1)/beta(j);
+      T(j,j) = alpha(j);
+      T(j+1,j) = gamma(j);
+      if j ~= k
+        T(j, j+1) = beta(j);
+      end
+  end
+  
+  ireject = 0;
+  while ireject <= mxrej % Finite number of rejections
+     mx = mb + k1;
+     F = expm(sgn*t_step*T(1:mx,1:mx));
+     if k1 == 0
+	      err_loc = btol; 
+        break;
+     else
+        phi1 = abs( beta*F(m+1,1) );
+        phi2 = abs( beta*F(m+2,1) * avnorm );
+        if phi1 > 10*phi2
+           err_loc = phi2;
+           xm = 1/m;
+        elseif phi1 > phi2
+           err_loc = (phi1*phi2)/(phi1-phi2);
+           xm = 1/m;
+        else
+           err_loc = phi1;
+           xm = 1/(m-1);
+        end
+     end
+     if err_loc <= delta * t_step*tol
+        break;
+     else
+        t_step = gamma * t_step * (t_step*tol/err_loc)^xm;
+        s = 10^(floor(log10(t_step))-1);
+        t_step = ceil(t_step/s) * s;
+        if ireject == mxrej
+           error('The requested tolerance is too high.');
+        end
+        ireject = ireject + 1;
+     end
+  end
+  mx = mb + max( 0,k1-1 );
+  w = V(:,1:mx)*(beta*F(1:mx,1));
+  beta = norm( w );
+  hump = max(hump,beta);
+
+  t_now = t_now + t_step;
+  t_new = gamma * t_step * (t_step*tol/err_loc)^xm;
+  s = 10^(floor(log10(t_new))-1); 
+  t_new = ceil(t_new/s) * s;
+
+  err_loc = max(err_loc,rndoff);
+  s_error = s_error + err_loc;
+end
+err = s_error;
+hump = hump / normv;
