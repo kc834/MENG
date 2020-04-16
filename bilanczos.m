ada8e06a078bb569b985e2d59b4039470bc527e7
Kevin Chaudhari <kevin@Kevins-MBP.home>
2020-04-16 17:02:55 -0400

    Bilanczos satisfies recurrence relations but the V and U matrices are not orthogonal

--- bilanczos.m
+++ bilanczos.m
@@ -12,14 +12,14 @@
 % Derived from lecture notes of David Bindel, CS 6210, Fall 2019, Cornell
 % Algorithm from PhD Thesis of Axel Facius, July 2000, Universitat Karlsruhe 
 %
-function [Q,R,alpha,beta,gamma] = bilanczos(A,b,k)
+function [Q,R,T] = bilanczos(A,b,k)
  
+
+
 n = length(A); % Max dim
 Q = zeros(n,k+1); % Orthonormal basis, n by k+1 array
 R = zeros(n,k+1);
-alpha = zeros(k,1); 
-beta = zeros(k,1);
-gamma = zeros(k,1);
+T = zeros(k+1, k+1);
 
 Q(:,1) = b/norm(b); % Arbitrary vector with norm 1
 Q(:,1) = Q(:,1)/norm(Q(:,1));
@@ -27,17 +27,85 @@ R(:,1) = Q(:,1);
 for j = 1:k 
     Q(:,j+1) = A*Q(:,j); % Move on to next vector in Krylov subspace
     R(:,j+1) = A'*R(:,j);
-    alpha(j) = R(:,j)'*Q(:,j+1);
-    Q(:,j+1) = Q(:,j+1)-alpha(j)*Q(:,j); 
-    R(:,j+1) = R(:,j+1)-alpha(j)*R(:,j); 
+    T(j,j) = R(:,j)'*Q(:,j+1);
+    Q(:,j+1) = Q(:,j+1)-T(j,j)*Q(:,j); 
+    R(:,j+1) = R(:,j+1)-T(j,j)*R(:,j); 
     if j > 1 
-        Q(:,j+1) = Q(:,j+1)-beta(j-1)*Q(:,j-1); 
-        R(:,j+1) = R(:,j+1)-gamma(j-1)*R(:,j-1); 
+        Q(:,j+1) = Q(:,j+1)-T(j-1, j)*Q(:,j-1); 
+        R(:,j+1) = R(:,j+1)-T(j, j-1)*R(:,j-1); 
     end
-    gamma(j) = norm(Q(:,j+1));
-    Q(:,j+1) = Q(:,j+1)/gamma(j);  
-    beta(j) = R(:,j+1)'*Q(:,j+1);
-    R(:,j+1) = R(:,j+1)/beta(j);
+    
+    T(j+1,j) = norm(Q(:,j+1));
+    if T(j+1,j) < 1e-7
+        break;
+    end
+    T(j,j+1) = R(:,j+1)'*Q(:,j+1);
+    if T(j,j+1) < 1e-7
+        break;
+    end
+    Q(:,j+1) = Q(:,j+1)/T(j+1,j);
+    R(:,j+1) = R(:,j+1)/T(j,j+1);
 end
+
+disp(Q);
+disp(R);
+disp(T);
+disp(Q(:, 1:k+1)*T(1:k+1, 1:k));
+disp(A*Q(:, 1:k));
+eqQ = all(ismembertol(Q(:, 1:k+1)*T(1:k+1, 1:k),A*Q(:, 1:k), 1e-7), 'all');
+if eqQ
+    disp("good eqQ");
+else
+    disp("bad eqQ");
+end
+
+disp(R(:, 1:k+1)*T(1:k, 1:k+1)');
+disp(A'*R(:, 1:k));
+eqR = all(ismembertol(R(:, 1:k+1)*T(1:k, 1:k+1)',A'*R(:, 1:k), 1e-7), 'all');
+if eqR
+    disp("good eqR");
+else
+    disp("bad eqR");
+end
+
+disp(Q(:, :)'*R(:, :));
+disp(R(:, :)'*Q(:, :));
+eqI = all(ismembertol(Q(:, 1:k)'*R(:, 1:k),R(:, 1:k)'*Q(:, 1:k), 1e-7), 'all');
+if eqI
+    disp("good eqI");
+else
+    disp("bad eqI");
+end
+
+% Either the last column of Q and R are wrong
+% Or 
+
+%  ======================== OLD CODE =======================
+
+% n = length(A); % Max dim
+% Q = zeros(n,k+1); % Orthonormal basis, n by k+1 array
+% R = zeros(n,k+1);
+% alpha = zeros(k,1); 
+% beta = zeros(k,1);
+% gamma = zeros(k,1);
+% 
+% Q(:,1) = b/norm(b); % Arbitrary vector with norm 1
+% Q(:,1) = Q(:,1)/norm(Q(:,1));
+% R(:,1) = Q(:,1);
+% for j = 1:k 
+%     Q(:,j+1) = A*Q(:,j); % Move on to next vector in Krylov subspace
+%     R(:,j+1) = A'*R(:,j);
+%     alpha(j) = R(:,j)'*Q(:,j+1);
+%     Q(:,j+1) = Q(:,j+1)-alpha(j)*Q(:,j); 
+%     R(:,j+1) = R(:,j+1)-alpha(j)*R(:,j); 
+%     if j > 1 
+%         Q(:,j+1) = Q(:,j+1)-beta(j-1)*Q(:,j-1); 
+%         R(:,j+1) = R(:,j+1)-gamma(j-1)*R(:,j-1); 
+%     end
+%     gamma(j) = norm(Q(:,j+1));
+%     Q(:,j+1) = Q(:,j+1)/gamma(j);  
+%     beta(j) = R(:,j+1)'*Q(:,j+1);
+%     R(:,j+1) = R(:,j+1)/beta(j);
+% end
 
 end
--- lanczos.m
+++ lanczos.m
@@ -10,23 +10,58 @@
 % 
 % Taken from lecture notes of David Bindel, CS 6210, Fall 2019, Cornell
 %
-function [Q,alpha,beta] = lanczos(A,b,k)
- 
+% function [Q,alpha,beta] = lanczos(A,b,k)
+function [Q,T] = lanczos(A,b,k)
+
+% n = length(A); % Max dim
+% Q = zeros(n,k+1); % Orthonormal basis, n by k+1 array 
+% alpha = zeros(k,1); 
+% beta = zeros(k,1);
+% 
+% Q(:,1) = b/norm(b); % Arbitrary vector with norm 1
+% for j = 1:k 
+%     Q(:,j+1) = A*Q(:,j); % Move on to next vector in Krylov subspace 
+%     alpha(j) = Q(:,j)'*Q(:,j+1);  
+%     Q(:,j+1) = Q(:,j+1)-alpha(j)*Q(:,j); 
+%     if j > 1 
+%         Q(:,j+1) = Q(:,j+1)-beta(j-1)*Q(:,j-1); 
+%     end
+%     beta(j) = norm(Q(:,j+1));
+%     Q(:,j+1) = Q(:,j+1)/beta(j); 
+% end
+
 n = length(A); % Max dim
 Q = zeros(n,k+1); % Orthonormal basis, n by k+1 array 
-alpha = zeros(k,1); 
-beta = zeros(k,1);
+T = zeros(k+1, k);
+% alpha = zeros(k,1); 
+% beta = zeros(k,1);
 
 Q(:,1) = b/norm(b); % Arbitrary vector with norm 1
 for j = 1:k 
     Q(:,j+1) = A*Q(:,j); % Move on to next vector in Krylov subspace 
-    alpha(j) = Q(:,j)'*Q(:,j+1);  
-    Q(:,j+1) = Q(:,j+1)-alpha(j)*Q(:,j); 
+    T(j,j) = Q(:,j)'*Q(:,j+1);  
+    Q(:,j+1) = Q(:,j+1)-T(j,j)*Q(:,j); 
     if j > 1 
-        Q(:,j+1) = Q(:,j+1)-beta(j-1)*Q(:,j-1); 
+        Q(:,j+1) = Q(:,j+1)-T(j,j-1)*Q(:,j-1); 
+        T(j-1,j) = T(j,j-1);
     end
-    beta(j) = norm(Q(:,j+1));
-    Q(:,j+1) = Q(:,j+1)/beta(j); 
+    if norm(Q(:, j+1)) < 1e-7 
+        break;
+    end
+    
+    T(j+1,j) = norm(Q(:,j+1));
+    Q(:,j+1) = Q(:,j+1)/T(j+1,j); 
+end
+
+% disp(Q);
+% disp(T);
+disp(Q*T);
+disp(A*Q(:, 1:k));
+eq = all(ismembertol(Q*T,A*Q(:, 1:k), 1e-7), 'all');
+if eq
+    disp("good");
+else
+    disp("bad");
 end
 
 end
